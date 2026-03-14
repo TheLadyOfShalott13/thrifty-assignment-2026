e46a5cec58a58a840f8531d3bd8f20644ba47464
Nisreen K. <angel.locsin.fan95@gmail.com>
2026-03-14 08:37:07 +0400

    importing banner image to avoid cors issues when deploying to github pages (even though this works locally)

--- app/components/hero-banner.tsx
+++ app/components/hero-banner.tsx
@@ -4,7 +4,7 @@ export default function HeroBanner() {
    return (
       <div className="absolute inset-0 w-full before:absolute before:inset-0 before:z-[1] before:bg-black/50">
          <Image
-            src="https://fcdn.thriftyuae.com/media/assets/home-page-banner.jpg"
+            src={`images/home-page-banner.jpg`}
             alt="Thrifty UAE"
             fill
             className="object-cover object-center"
