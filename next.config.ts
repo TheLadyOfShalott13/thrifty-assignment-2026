b8ae01cd6539e6ca797714191e7404e384bc9185
Nisreen K. <angel.locsin.fan95@gmail.com>
2026-03-14 07:52:54 +0400

    remove unnecessary base path - github actions handling this already

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = process.env.NEXT_PUBLIC_BASE_PATH;
-
 const nextConfig: NextConfig = {
    output: "export",
-   basePath: repoName ? `/${repoName}` : "",
-   assetPrefix: repoName ? `/${repoName}/` : "",
    images: {
       unoptimized: true,
       remotePatterns: [
