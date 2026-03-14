0d2c8ea220315afe6e9a7c883f0db97433aeb684
Nisreen K. <angel.locsin.fan95@gmail.com>
2026-03-14 08:28:18 +0400

    prod vs dev environments for path resolution for images

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.IS_PRODUCTION === "true";
+const base = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
    output: "export",
-   basePath: '/thrifty-assignment-2026',
-   assetPrefix: '/thrifty-assignment-2026/',
+   basePath: isProd ? `/${base}` : "",
+   assetPrefix: isProd ? `/${base}/` : "",
    images: {
       unoptimized: true,
       remotePatterns: [
